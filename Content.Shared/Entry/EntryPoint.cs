ce4631664a541eae80bfc061f1154b6bea7e4fe5
CattyLucky <mpalhozaev@gmail.com>
2026-02-19 20:05:14 +0500

    Перевод

--- Content.Shared/Entry/EntryPoint.cs
+++ Content.Shared/Entry/EntryPoint.cs
@@ -1,5 +1,4 @@
 using System.Diagnostics.CodeAnalysis;
-using System.Globalization; // WL Change
 using System.IO;
 using System.Linq;
 using Content.Shared.Humanoid.Markings;
@@ -45,7 +44,6 @@ namespace Content.Shared.Entry
         public override void PostInit()
         {
             base.PostInit();
-            IoCManager.Resolve<ILocalizationManager>().SetCulture(new CultureInfo("ru-RU")); //WL Change
             InitTileDefinitions();
             Dependencies.Resolve<MarkingManager>().Initialize();
 
